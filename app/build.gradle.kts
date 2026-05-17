efec2bce56bec4235946747a841d280c2a100109
vpapaion <63016422+vpapaion@users.noreply.github.com>
2026-05-17 12:18:19 +0300

    Add app build file

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application") }
+
+android { namespace = "gr.vaios.demo"; compileSdk = 35
+    defaultConfig { applicationId = "gr.vaios.demo"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+}
